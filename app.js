336f4353e06114fbf2d234fcd13fe8f452b32d3b
= <=>
2017-11-25 20:17:01 +0530

    login and register

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 // New Code
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/ADC');
+var db = monk('localhost:27017/adc');
 
 
 var index = require('./routes/index');
--- routes/patient.js
+++ routes/patient.js
@@ -6,9 +6,80 @@ router.get('/', function(req, res, next) {
   res.render('patient',{title:'Patient Module'});
 });
 
-router.get('/addpatient', function(req, res) {
+
+router.post('/login',function(req,res,next){
+
+    console.log(req.body)
+    var db = req.db;
+    console.log("here is login")
+    // Get our form values. These rely on the "name" attributes
+    var patientName = req.body.pname;
+    var password = req.body.patientPassword;
+    var collection = db.get('patient');
+    collection.find({},{},function(e,docs){
+        console.log('here is the login');
+        res.render('patientlist', {
+            "patientlist" : docs
+        });
+         console.log('here is the patient',docs);
+    });
+     // collection.find({
+    //     "p_name" : patientName,
+    //     "password" : password,
+    // }, function (err, doc) {
+    //     if (err) {
+    //         // If it failed, return error
+    //         res.send("There was a problem adding the information to the database.");
+    //     }
+    //     else {
+    //         // And forward to success page
+    //         res.render('patientlist', { title: 'Login Patient' },{
+    //             'patientData' : doc
+    //         });
+    //     }
+    // });
+});
+
+router.post('/', function(req, res) {
+    console.log("hello data")
+    console.log(req.body)
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var patientName = req.body.pname;
+    var patientAge = req.body.age;
+    var patientMobile =req.body.pmobile;
+    var password = req.body.patientPassword;
+    var cpassword = req.body.cpatientPassword;
+
+    // Set our collection
+    var collection = db.get('patient');
+
+    // Submit to the DB
+    collection.insert({
+        "p_name" : patientName,
+        "age" : patientAge,
+        "mobile":patientMobile,
+        "password" : password,
+        "cpassword" : cpassword
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.render('loginPatient', { title: 'Login Patient' });
+        }
+    });
+});
+
+router.get('/registerPatient', function(req, res) {
     res.render('newpatient', { title: 'Add New Patient' });
 });
+
 
 /* POST to Add User Service */
 router.post('/addp', function(req, res) {
@@ -47,7 +118,7 @@ router.get('/list', function(req, res) {
     var db = req.db;
     var collection = db.get('patient');
     collection.find({},{},function(e,docs){
-        res.render('patientlist', {
+        res.render('loginPatient', {
             "patientlist" : docs
         });
     });
