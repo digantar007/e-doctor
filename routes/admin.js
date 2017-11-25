4bebf72b903260c74dfef1a3a13706489c7c9c57
= <=>
2017-11-25 17:01:05 +0530

    initial commit

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('doctor', { title: 'doctor' });
 });
 
 module.exports = router;
