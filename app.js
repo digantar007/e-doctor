c9d85231eef342f2d3a10622f6b54a8c97f14ab5
digantar007 <=>
2017-11-26 15:14:08 +0530

    added datatables

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var $ = require("jquery");
+
 
 // New Code
 var mongo = require('mongodb');
